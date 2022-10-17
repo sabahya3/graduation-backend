f0d01f6ce6eeb673b30b03cf36a7d2e520c2b329
sabahya3 <sabahya3@gmail.com>
2022-10-17 18:31:32 +0200

    first commit2

---
+++ models/student.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+const Shema = mongoose.Schema
+const { ObjectId } = mongoose.Schema.Types;
+
+
+const studentSchema = new Shema({
+    studentName: {
+        type: String,
+        reqired: true
+    },
+    nameOfFather: {
+        type: String,
+        reqired: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+
+    familyPhoneNumbers: [{
+        type: String,
+
+    }],
+
+    studentAge: { type: Number, reqired: true },
+
+    gendre: [{ type: String, enum: ['Male', 'Female'] }],
+
+    grade: { type: ObjectId, ref: 'Grade' },
+
+    accessToken: {
+        type: String,
+
+    },
+
+
+    phoneToken: {
+        type: String,
+
+    },
+
+    chats: [{ type: ObjectId, ref: 'Teacher' }]
+},
+
+    { timestamps: true })
+
+
+
+
+module.exports = mongoose.model('Student', studentSchema);
